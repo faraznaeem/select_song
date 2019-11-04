bacd746cd92f88e897fbed2d02760ab6f9a0c3dd
faraznaeem <faraznaeem@gmail.com>
2019-11-04 21:33:10 +0100

    Creates selected song action

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+// We are using a named export in this file like the one below, 
+// and the reason for that is so we can export multiple things from this file.
+// Otherwise we would have to create multiple files
+// this also means that we need to import the component 
+// with curly braces like this { selectedSong }
+
+export const selectedSong = song => {
+  return {
+    type: "SELECTED_SONG",
+    payload: song
+  };
+};
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { selectedSong } from "../actions/";
 const App = () => {
   return <div>Hello</div>;
 };
