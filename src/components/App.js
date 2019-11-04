635ee8457d2589e24c08f64492917d8e285b9567
faraznaeem <faraznaeem@gmail.com>
2019-11-04 22:06:19 +0100

    Creates song list component and renders it in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { selectedSong } from "../actions/";
+import SongList from "./SongList";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
