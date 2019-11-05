0b1982f1032bc9e0dd915e4544fd84077fa74e33
faraznaeem <faraznaeem@gmail.com>
2019-11-05 20:09:10 +0100

    Adds song list to app component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button blue pimary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    return <div className="ui dividied list">{this.renderList()}</div>;
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
